69161689e2743245c46092e9190de76eb81b9ccc
kim <kim@DESKTOP-1REGLAL>
2021-07-21 09:04:57 +0900

    연습

--- JavaEx/JavaEx/src/com/practice01/Practice01_02.java
+++ JavaEx/JavaEx/src/com/practice01/Practice01_02.java
@@ -5,6 +5,8 @@ public class Practice01_02 {
 	public static void main(String[] arg){
 		Scanner sc = new Scanner(System.in);
 		int i = sc.nextInt();
+		
+		
 		for(int j=1;j<=i;j++) {
 			for(int z=0;z<j;z++) {
 				System.out.print(j);
--- JavaEx/JavaEx/src/com/practice01/Problem03.java
+++ JavaEx/JavaEx/src/com/practice01/Problem03.java
@@ -4,14 +4,49 @@ public class Problem03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=1;i<10;i++)
-		{
-			for(int j=2;j<10;j++)
+//		for(int i=1;i<10;i++)
+//		{
+//			for(int j=2;j<10;j++)
+//			{
+//				System.out.print(j+"*"+i+"="+i*j+"\t");
+//			}
+//			System.out.println();
+//		}
+//		int i=1;
+//		int j=2;
+//		while(true) {		
+//			while(true)
+//			{
+//				System.out.print(j+"*"+i+"="+i*j+"\t");
+//				j++;
+//				if(j>9)
+//				{
+//					j=2;
+//					break;
+//				}
+//				
+//			}	
+//			System.out.println();
+//			i++;
+//			if(i>9)
+//				break;
+//		}
+		
+		int i=1;
+		int j=2;
+		while(i<10) {		
+			while(j<10)
 			{
 				System.out.print(j+"*"+i+"="+i*j+"\t");
-			}
+				j++;
+			}	
+			if(j>9)//초기화는 	밖에서!
+				j=2;
 			System.out.println();
+			i++;
+	
 		}
+		
 	}
 
 }
